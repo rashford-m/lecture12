c0e4542b040010192cc67b5a6a12105bf44d5af6
Rashford <mikearowo@gmail.com>
2023-02-13 21:56:36 +0100

    updated

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,13 +1,12 @@
-import { userQuery } from "@tanstack/react-query";
-import Axios from "axios";
+import { useGetCat } from "../useGetCat";
 
 export const Cat = () => {
-  const { data: catData } = userQuery(["cat"], async () => {
-    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-  });
+  const { data, isCatLoading, refetchData } = useGetCat();
+  if (isCatLoading) return <h1>Loading...</h1>;
   return (
     <div>
-      <h1>{catData?.fact}</h1>
+      <button onClick={refetchData}>refetch</button>
+      <h1>{data?.fact}</h1>
     </div>
   );
 };
